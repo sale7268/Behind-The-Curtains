c088f1d3a23072f98638cfc75bacc25ceb63b5e1
Rocco <roccoestrada@gmail.com>
2020-12-02 08:42:40 -0800

    Looping effect added. Scene nearly complete.

---
+++ Behind the curtains/Assets/Rocco/LostWoodsEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostWoodsEffect : MonoBehaviour
+{
+    public GameObject room;
+    public GameObject initialHall;
+    public GameObject roomSpawn;
+
+
+     private void OnTriggerEnter(Collider other)
+    {
+        initialHall.transform.position = transform.parent.position;
+        initialHall.transform.rotation = transform.parent.rotation;
+        Destroy(room);
+        Instantiate(room, roomSpawn.transform.position, roomSpawn.transform.rotation);
+    }
+}
