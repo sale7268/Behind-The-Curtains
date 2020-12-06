5fe2f72ab8f3f2847a9bc1c0b3250b3b67e72eb5
Rocco <roccoestrada@gmail.com>
2020-12-06 00:50:52 -0800

    Cleaned up scene and added hints

---
+++ Behind the curtains/Assets/Rocco/Scripts/ModUI_Interaction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModUI_Interaction : MonoBehaviour
+{
+
+    [Header("Interaction UI State")]
+    [SerializeField] private GameObject GreenlandShark;
+    [SerializeField] private GameObject Honey;
+    [SerializeField] private GameObject Trees;
+
+    public void Continue()
+    {
+        UIDisable();
+    }
+    public void UIDisable()
+    {
+        GreenlandShark.SetActive(false);
+        Honey.SetActive(false);
+        Trees.SetActive(false);
+
+        Time.timeScale = 1;
+        Screen.lockCursor = true;
+    }
+}
